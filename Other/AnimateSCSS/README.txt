f27fb98997932672662506f09c516e84dc07c298
Aintik <100485252+Aintik@users.noreply.github.com>
2025-10-12 15:42:43 +0100

    uploaded scss animations project

---
+++ Other/AnimateSCSS/README.txt
@@ -0,0 +1,7 @@
+Used:
+	Parcel
+	SCSS
+	SCSS moduls
+	SCSS mixins
+	AOS animate
+	connecting local fonts
---
+++ Other/AnimateSCSS/app.js
@@ -0,0 +1,17 @@
+import './styles/app.scss';
+import AOS from 'aos';
+
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    disable: 'mobile'
+  });
+})
+
+
+
+
+
+
+
+
+console.log('Congrats');
